5f07c3b1d6ba0dd6e302187b883183797e104a5b
Dungna89 <dungna29@fpt.edu.vn>
2021-09-14 16:15:13 +0700

    1

---
+++ 7_IT17322_COM108_FALL21/Bai_1.0_GioiThieuLapTrinhC/Main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+/*
+ *	Làm quen cơ bản
+ *	1. #include <stdio.h>: Import thư viện C vào
+ *	2. Một chương trình phải có hàm main()
+ *	3. 1 Câu lệnh phải kết thúc bằng dấu ;
+ *	4. Ctrl + F5: Chạy chương trình
+ *	5. Ctrl + Space: Dùng để gợi ý code trong IDE	
+ *
+ *	2 Loại người học lập trình:
+ *		1. Code đúng nghĩa
+ *		2. Chép code
+ */
+int main()//Hàm main
+{//Mở hàm
+	//Bên trong 1 hàm gọi Body Code là phần các bạn sẽ code ở đây
+	printf("Chao Cac Ban Den Voi Lap Trinh C");	
+}//Đóng hàm
