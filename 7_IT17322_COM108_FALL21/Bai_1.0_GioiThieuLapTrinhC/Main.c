ca0e378b6bf89517bf10b8458070a7e30a7d3e19
Dungna89 <dungna29@fpt.edu.vn>
2021-09-16 16:16:19 +0700

    1

--- 7_IT17322_COM108_FALL21/Bai_1.0_GioiThieuLapTrinhC/Main.c
+++ 7_IT17322_COM108_FALL21/Bai_1.0_GioiThieuLapTrinhC/Main.c
@@ -5,14 +5,23 @@
  *	2. Một chương trình phải có hàm main()
  *	3. 1 Câu lệnh phải kết thúc bằng dấu ;
  *	4. Ctrl + F5: Chạy chương trình
- *	5. Ctrl + Space: Dùng để gợi ý code trong IDE	
+ *	5. Ctrl + Space: Dùng để gợi ý code trong IDE
+ *	6. Ctrl + D: sao chép 1 hoặc nhiều dòng code
+ *	7. Ctrl + K + D: Format code - Clean Code
+ *	8. Ctrl + K + C: Comment code
+ *	9. Ctrl + K + U: Mở Comment code
  *
  *	2 Loại người học lập trình:
  *		1. Code đúng nghĩa
  *		2. Chép code
  */
-int main()//Hàm main
+ // MỖI PROJECT CHỈ ĐƯỢC PHÉP CÓ 1 HÀM MAIN
+int main()//Hàm main và có ký hiệu () gọi là hàm
 {//Mở hàm
 	//Bên trong 1 hàm gọi Body Code là phần các bạn sẽ code ở đây
-	printf("Chao Cac Ban Den Voi Lap Trinh C");	
+	printf("Chao Cac Ban Den Voi Lap Trinh C");
+	printf("Chao Cac Ban Den Voi Lap Trinh C");
+	printf("Chao Cac Ban Den Voi Lap Trinh C");
+	printf("Chao Cac Ban Den Voi Lap Trinh C");
+	printf("Chao Cac Ban Den Voi Lap Trinh C");
 }//Đóng hàm
---
+++ 7_IT17322_COM108_FALL21/Bai_1.1_KhaiNiemVeBien/Main.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+/*
+ * BÀI 1.1 KHÁI NIỆM VỀ BIẾN
+ * 1. Biến dùng để lữu trữ giá trị trong lập trình
+ * 2. Biến được lưu vào RAM máy tính khi chương trình bắt đầu chạy và sẽ mất đi khi dừng chương trình
+ * 3. Biến cần phải có kiểu dữ liệu ví dụ: int, float, char......
+ */
+int main()
+{
+	/*
+	 * Phần 1: Công thức khai báo biến
+	 * + <Kiểu dữ liệu> <Tên biến>;//Cách khai báo 1
+	 * + <Kiểu dữ liệu> <Tên biến 1>,<Tên biến 2>,<Tên biến 3>;//Khai báo nhiều biến có cùng kiểu dữ liệu
+	 * + <Kiểu dữ liệu> <Tên biến> =(toán tử gán) <giá trị>;
+	 *
+	 * <Kiểu dữ liệu>: Dùng để định nghĩa kiểu dữ liệu cho giá giá trị cần lưu trữ.
+	 */
+	//Ví dụ:	
+	int a;//Khai báo 1 biến a có kiểu dữ liệu là số nguyên và không khởi tạo giá trị ban đầu
+	a = 1;//Gán giá trị cho biến a thông qua toán tử =
+
+	int b = 9;//Khai báo 1 biến b có kiểu dữ liệu là số nguyên và có khởi tạo giá trị ban đầu
+
+	int b1, b2, b3, b4 = 10;//Khai báo nhiều biến có cùng kiểu dữ liệu
+
+	int tuoi = 18;
+
+	/*
+	 * Phần 2: Đặt tên biến
+	 * - Tên biến đặt dễ hiểu và đúng nghĩa
+	 * - Tên biến hạn chế đặt bằng tiếng việt khi đi làm về sau
+	 * - Không đặt tên biến gây hiểu nhầm.
+	 * Gồm 3 cách đặt tên biến với 2 từ trở lên
+	 * - C1: normal: sinhvien - thông thường tên biến sẽ đặt kiểu nomarl
+	 * - C2: CamelCase: SinhVien
+	 * - C3: Under_Score: sinh_Vien
+	 */
+
+	 /*
+	  * Phần 3: Các kiểu dữ liệu trong lập trình
+	  * Số nguyên: short,long, int
+	  * Số thực: float,double
+	  * Ký tự: char (Chỉ có thể lưu trữ được 1 ký tự)
+	  * Chuỗi: String (Ngôn ngữ C không có String)
+	  * Logic: Boolean trả ra True hoặc False nhưng trong C sẽ trả ra 0 và 1
+	  */
+	short temp = 32767;
+	char c1 = '5';
+	char c2 = 'a';
+	char c3 = '+';
+	
+}
