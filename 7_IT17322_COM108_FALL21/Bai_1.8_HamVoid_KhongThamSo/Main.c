be8bc5c4afeabd0e0740ef93516c5938ab4078fa
Dungna89 <dungna29@fpt.edu.vn>
2021-10-02 15:46:17 +0700

    1

--- 7_IT17322_COM108_FALL21/Bai_1.8_HamVoid_KhongThamSo/Main.c
+++ 7_IT17322_COM108_FALL21/Bai_1.8_HamVoid_KhongThamSo/Main.c
@@ -14,12 +14,25 @@
   }
 */
 int a, b;//Đưa a b ra làm biến toàn cục để các hàm có thể dùng chung
+
+//Hàm void không tham số
 void nhapHaiSo() {//Hàm do dev tự định nghĩa	
 	printf("Moi ban nhap a: ");
 	scanf_s("%d", &a);
 	printf("Moi ban nhap b: ");
 	scanf_s("%d", &b);
 }
+
+//Hàm void có tham số: Có thể có bao nhiêu biến tùy theo bài toán và cũng có thể có các kiểu dữ liệu khác nhau.
+void phepTru(int a,int b)//(int a,int b) được gọi là tham số của hàm.
+{
+	//Các tham số được sử dụng bên trong hàm hoàn toàn bình thường.
+	printf("%d - %d = %d", a, b, a - b);
+}
+
+//Thông thường chúng ta chỉ nên khai báo hàm trong C như cách sau:
+void hamChucNang1();//Khai báo hàm chứ chưa code bên trong với mục đích đặt hàm này nằm dưới main cho dễ quản lý code
+void hamChucNang2(int a, int b, int c, double d, float e, int arrnumber[]);
 int main() {
 	char choice;	
 	printf("1. Phep Cong \n");
@@ -37,7 +50,9 @@ int main() {
 		break;
 	case '2':	
 		nhapHaiSo();//Gọi tên hàm và kết thúc bằng dấu ();
-		printf("%d - %d = %d", a, b, a - b);
+		//Khi sử dụng hàm có tham số lưu ý phải truyền đúng giá trị vào các vị trí trên tham số của hàm và đúng kiểu dữ liệu.
+		//phepTru(); Báo đỏ là vì chưa truyền tham số vào cho hàm
+		phepTru(b,a);//Truyền tham số đúng kiểu dữ liệu
 		break;
 	case '3':		
 		nhapHaiSo();//Gọi tên hàm và kết thúc bằng dấu ();
@@ -50,4 +65,8 @@ int main() {
 	default:
 		break;
 	}
+}
+void hamChucNang1()
+{
+	//Sau khi đã khai báo ở trên tiến hành copy xuống và triển khai code như bình thường
 }
---
+++ 7_IT17322_COM108_FALL21/Bai_1.9_HamTraVe/Main.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+/*
+* Hàm trả về - trả về các giá trị
+*	1. Không tham số
+*	2. Có tham số
+* Cách sử dụng:
+*	1. Tên hàm phải có nghĩa
+*	2. Tên hàm luôn có động từ đứng trước:
+*		Ví dụ: tinhTong(),layDsSinhVien, getLstSinhVien.....
+*
+  Công thức:
+  <Kiểu dữ liệu trả về> <Tên hàm> (<Các tham số truyền vào>)
+  {
+		//Body code
+		return <Đúng Kiểu dữ liệu của hàm>;//Luôn nằm ở cuối cùng
+  }
+*/
+//Khai báo hàm không triển khai trước main
+int phepTru(int a,int b);
+int phepTru2();
+int main()
+{
+	//Cách gọi hàm trả về: cần phải biết tên và coi hàm trả về là 1 giá trị
+	phepTru(10, 5);//Không thấy in ra màn hình bởi cả hàm là 1 giá trị muốn in ra thì phải đưa nó vào câu lệnh in
+	printf("%d", phepTru(10, 5) +2000);
+	printf("%d", phepTru2());
+	
+}
+
+int phepTru(int a, int b)
+{
+	return a - b;//Là 2 số nguyên trừ cho nhau sẽ trả ra 1 giá trị nguyên
+}
+int phepTru2()
+{
+	int a, b;
+	printf("Moi ban nhap a: ");
+	scanf_s("%d", &a);
+	printf("Moi ban nhap b: ");
+	scanf_s("%d", &b);
+	return a - b;
+}
+int getYear()
+{
+	return 2021;
+}
+char getChar()
+{
+	return 'a';
+}
+float getfloat()
+{
+	return 5.6;
+}
